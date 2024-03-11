692ecf1e46407ab11a232ecf897c1f98af28239d
EleNdor <amo123beach@gmail.com>
2024-03-11 19:05:16 +0300

    1

---
+++ toDoList2/app.js
@@ -0,0 +1,17 @@
+const list_1 = document.getElementById('firstContent')
+const list_2 = document.getElementById('secondContent')
+const list_3 = document.getElementById('thirdContent')
+const list_4 = document.getElementById('fourthContent')
+const creater = document.getElementById('creater')
+const checkerTime = document.getElementById('time')
+const checkerImportance = document.getElementById('importance')
+const note = document.getElementById('inputText')
+let ischeckerTime = checkerTime.checked
+let ischeckerImportance = checkerImportance.checked
+
+
+creater.onclick = function(){
+    console.log(ischeckerTime,note.value,ischeckerImportance)
+    note.value = ''
+}
+console.log(ischeckerTime,note.value)
